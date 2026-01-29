df0d632fdde3d525bf0dd78355ecedd7074bcefe
skpolanowice-sourceskp <skpolanowice@gmail.com>
2026-01-29 11:58:45 +0100

    Add files via upload

---
+++ reviews_data.js
@@ -0,0 +1,62 @@
+const reviewsData = [
+    {
+        name: "Pawel Bawer",
+        text: "Polecam, bardzo profesjonalna obsługa. Diagnosta oprócz standardowego przeglądu, zwraca uwagę na co zwrócić uwagę w najbliższym czasie.",
+        stars: 5,
+        service: "Przegląd"
+    },
+    {
+        name: "Piotrek R.",
+        text: "Z pełnym przekonaniem polecam stację diagnostyczną SSCAR. Badanie techniczne jest tu zawsze przeprowadzane bardzo dokładnie i rzetelnie. Dużym plusem jest profesjonalne podejście do klienta – diagnosta nie tylko wykonuje badanie, ale także jasno informuje, na co warto już teraz zwrócić uwagę i co w najbliższym czasie może wymagać naprawy lub wymiany. Dzięki temu klient wychodzi z pełną wiedzą o stanie technicznym swojego pojazdu i może zaplanować dalsze działania bez niespodzianek. Obsługa na wysokim poziomie, fachowość i uczciwość – zdecydowanie miejsce godne polecenia.",
+        stars: 5,
+        service: "Badanie Techniczne"
+    },
+    {
+        name: "My wy Oni",
+        text: "Bardzo dobre i profesjonalne miejsce usterka w postaci zbieżności ustawiona od ręki i w rozsądnej cenie. Miejsce godne polecenia.",
+        stars: 5,
+        service: "Geometria"
+    },
+    {
+        name: "Daro Stachu",
+        text: "Polecam stacje bardzo profesjonalne podejście do klienta i przede wszystkim do auta ,światła ustawione przez diagnostę mała rzecz a cieszy, nie wszędzie tak jest .",
+        stars: 5,
+        service: "Ustawienie świateł"
+    },
+    {
+        name: "Рома Дядечко",
+        text: "Profesjonalne zrobiony przegląd,bardzo fajna atmosfera,doświadczony personal,dziękuję serdecznie. Polecam najlepsza stacja we Wrocławiu.",
+        stars: 5,
+        service: "Przegląd"
+    },
+    {
+        name: "Maciej Wasiluk",
+        text: "Zdecydowanie polecam tę stację jako miejsce na sprawdzenie auta przed zakupem. Diagnosta podzielił proces na 3 etapy. W pierwszym sam sprawdził auto. W drugim zaprosił mnie na hale i pokazał swoje wszytskie wątpliwości, pozwolił zajrzeć pod auto. W ostatniej części mogłem sam skonfrontować wszystkie bolączki auta z jego właścicielem. Pełen profesjonalizm, polecam kupującym auto. Handlarzom może niekoniecznie, bo nic się tutaj nie ukryje.",
+        stars: 5,
+        service: "Sprawdzenie auta"
+    },
+    {
+        name: "Jolanta Dobrzeniecka",
+        text: "Dziękuję za profesjonalne przeprowadzenie przeglądu i przemiłą obsługę....to już mój kolejny raz kiedy przyjeżdżam na tę stację i na pewno tu wrócę , zarówno z motocyklem jak i z samochodem....polecam serdecznie...",
+        stars: 5,
+        service: "Przegląd"
+    },
+    {
+        name: "Kamil Tkacz",
+        text: "Miałem robiony przegląd w czerwcu oraz niedawno geometrię. Bardzo przyjemne doświadczenie. Uprzejmi i profesjonalni pracownicy. Miejsce bardzo czyste, jest kawa i można nawet podładować telefon. Mocno polecam.",
+        stars: 5,
+        service: "Geometria"
+    },
+    {
+        name: "Volodymyr Musalian",
+        text: "Chciałbym serdecznie podziękować Panu Sebastianowi za profesjonalnie przeprowadzone badanie techniczne mojego samochodu. Cała procedura została wykonana sprawnie, rzetelnie i z dużą dokładnością. Pan Sebastian w sposób jasny i zrozumiały wyjaśnił wszystkie kwestie związane ze stanem technicznym pojazdu oraz zwrócił uwagę na szczegóły, które mogą mieć znaczenie w przyszłości. Widać, że podchodzi do swojej pracy z pełnym zaangażowaniem i doświadczeniem. Zdecydowanie polecam każdemu, kto szuka fachowej i uczciwej obsługi !!!",
+        stars: 5,
+        service: "Badanie Techniczne"
+    },
+    {
+        name: "Michał Grab",
+        text: "Pan Michał to prawdziwy profesjonalista! Badanie techniczne przebiegło sprawnie i bez żadnych problemów. Obsługa bardzo miła i życzliwa – od razu czuć, że klient jest tutaj ważny. Na miejscu dostępna jest poczekalnia, w której można napić się dobrej kawy i w komfortowych warunkach poczekać na zakończenie przeglądu. Cała stacja jest czysta i zadbana, co dodatkowo świadczy o podejściu do pracy. Pan Michał nie tylko dokładnie sprawdził samochód, ale też dopompował koła, co było bardzo miłym gestem. Serdecznie polecam tę stację kontroli pojazdów każdemu, kto ceni sobie profesjonalizm, rzetelność i przyjazną atmosferę! 🚗☕✨",
+        stars: 5,
+        service: "Przegląd"
+    }
+];
